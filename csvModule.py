c3eeb7f9f62873f2de016d7476b5618c503f0b46
leoliu <cubemail88@gmail.com>
2019-12-28 15:49:24 +0800

    finish get data info from csv

--- csvModule.py
+++ csvModule.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import os
 
+currentWorkSpace = os.getcwd()
+
 def write2csv(AllInfoList):
     # add '.csv' at the end of the current workspace path
-    dataFilePath = os.getcwd()+'/result.csv'
+    dataFilePath = currentWorkSpace+'/result.csv'
     titleList=['股號', '收盤價', '股票股利', '現金股利', '去年EPS', '今年EPS']
     # data must be 2d, other pd.DataFrame will meet exception
     df = pd.DataFrame(data=AllInfoList, columns=titleList)
@@ -14,10 +16,26 @@ def write2csv(AllInfoList):
 def mockwrite2csv():
     mockList = [['1402', '30', '0.8', '0.95', '2.41', '1.69'], ['2330', '338', '1.52', '2.28', '13.54', '8.84']]
      # add '.csv' at the end of the current workspace path
-    dataFilePath = os.getcwd()+'/result.csv'
+    dataFilePath = os.getcwd()+'/test/result.csv'
     titleList=['股號', '收盤價', '股票股利', '現金股利', '去年EPS', '今年EPS']
     # data must be 2d, other pd.DataFrame will meet exception
     df = pd.DataFrame(data=mockList, columns=titleList)
     # encoding to big5 for zh-tw
     df.to_csv(dataFilePath, encoding='big5', index=False)
     print ('Done!!')
+
+def readStockInfoFromCSV():
+    StockIdandNameList = []
+    dataFilePath = currentWorkSpace+'/stock.csv'
+    df = pd.read_csv(dataFilePath, encoding='utf-8', sep = "\t")
+    #get col 'Info' data and convert to list
+    StockIdandNameList = list(df.loc[:, 'Info'])
+    return StockIdandNameList
+
+def mockreadStockInfoFromCSV():
+    mydata = []
+    dataFilePath = currentWorkSpace+'/test/stock.csv'
+    df = pd.read_csv(dataFilePath, encoding='utf-8', sep = "\t")
+    #get col 'Info' data and convert to list
+    mydata = list(df.loc[:, 'Info']) 
+    return mydata
--- test.py
+++ test.py
@@ -1,3 +1,6 @@
 import csvModule
 
 csvModule.mockwrite2csv()
+
+# read from CSV and return StockInfoList
+# lista = csvModule.readStockInfoFromCSV()
